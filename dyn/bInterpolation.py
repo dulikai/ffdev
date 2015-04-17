39ebd5ce58fac3ae9677cd15fed23af41e633b63
dulikai <dulikai@foxmail.com>
2015-04-17 17:56:49 +0800

    linear interpolation ..

--- dyn/bInterpolation.py
+++ dyn/bInterpolation.py
@@ -25,24 +25,6 @@ class xyzRoom:
             vel = np.array([float(rec[4]), float(rec[5]), float(rec[6])])
         return name, coord
 
-    def singlePage(filename="abc.xyz"):
-        """
-        for a xyz file with only one xyz structure
-        """
-        fp = open(filename, "r")
-        n_site = int(fp.readline())
-        title = fp.readline().strip()
-        sites = [[]for i in xrange(n_site)]
-        for i in xrange(n_site):
-            line = fp.readline()
-            name, pos = self.line2vars(line)
-            sites[i].name = name
-            sites[i].pos = pos
-        fp.close()
-        # store vars
-        self.sites = sites
-        self.n_site = n_site
-        fp.close()
 
           
 
@@ -51,7 +33,54 @@ class bInterpolation():
         
         return
 
+    def line2site(line = "C 0.0 0.0 0.0"):
+        record = line.split()
+        vel = np.array([0.0, 0.0, 0.0])
+        name = record[0]
+        coord = [float(f) for f in record[1:4]]
+        coord = np.array(coord)
+
+        return name, coord
+
+    def singlexyz(filename = "abc.xyz"):
+        """ read in a xyz format file """
+        fp = open(filename, "r")
+        n_site = int(fp.readline())
+        title = fp.readline().strip()
+        sites = []
+        for i in xrange(n_site):
+            line = fp.readline()
+            record = line.split()
+            name = record[0]
+            coord = np.array([float(f) for f in record[1:4]])
+            sites.append({'name': name, 'coord': coord})
+        fp.close()
+             
+        return sites
+
+    def get_new_pos(self, pointA, pointB, i, n):
+        """
+        get the new position via linear interpolation
+        ith point from A, 
+        """
+        k = float(i) / n
+        p = pointA * (1 - k) + pointB * k
+        return p
+
+    def get_new_geom(self, beg, end, i, n):
+        """
+        given structural beg & end
+        generate n geometries
+        """
+        n_site = 0
+        sites = []
+        for xb, xe in zip(beg, end):
+            coordb = xb['coord']
+            
+
+        return
+
     
-    def read(filename=[]):
+    
 
     
